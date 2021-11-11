a5c6eb39f5629a1b649e0132d2531ab59793433c
Lane Anderson <melanieanderson1995@gmail.com>
2021-11-11 10:19:46 -0500

    wip adding events

--- src/components/pages/news.js
+++ src/components/pages/news.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import Typography from "@material-ui/core/Typography";
+import { Paper } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import Footer from '../footer';
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+        textAlign: "center",
+        padding: 20,
+        fontSize: 32,
+        color: "#f5f5f5"
+    },
+}))
 
 export default function News () {
+    const classes = useStyles();
+
     return (
-        <>
-            <Typography>
+        <Paper style={{ backgroundColor: "black" }}>
+            <div className={classes.header}>
                 News
-            </Typography>
-        </>
+            </div>
+            <div>
+                {events.map((event) => (
+                    <div>
+                        {event.name}
+                    </div>
+                ))}
+            </div>
+            <Footer/>
+        </Paper>
     )
 }
